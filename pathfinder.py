d45751e85901a0a32e108d62902dab5518aabf54
Vlad Kravchenko <vladkravchenkomail@gmail.com>
2020-05-02 21:25:59 +0100

    Some small refactoring

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,7 @@
 import collections
 
+
+
 class Grid():
     def __init__(self, width=5, height=5):
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
@@ -7,13 +9,19 @@ class Grid():
         self.width = width-1
         self.came_from = {}
         self.walls = []
-        self.path = []
         self.neighbours = {}
+        self.start = ()
+        self.goal = ()
+        self.path = []
+
+    def set_start_goal(self, start, goal):
+        self.start = start
+        self.goal = goal
 
     def create_wall(self, x, y):
         self.walls.append((x,y))
 
-    def is_inside_field(self, vertex):
+    def is_within_grid(self, vertex):
         x, y = vertex
         return 0 <= x <= self.width and 0 <= y <= self.height
 
@@ -24,23 +32,22 @@ class Grid():
         x=0
         for row in self.grid:
             y=0
-            for elem in row:
+            for vertex in row:
                 if (x, y) not in self.walls:
                     self.neighbours[x,y] = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
-                    self.neighbours[x,y] = list(filter(self.is_inside_field, self.neighbours[x,y]))
+                    self.neighbours[x,y] = list(filter(self.is_within_grid, self.neighbours[x,y]))
                     self.neighbours[x,y] = list(filter(self.is_passable, self.neighbours[x,y]))
                 y+=1
             x+=1 
 
-
-    def breadth_first_search(self, start):
+    def breadth_first_search(self):
         # breadth search algorithm for searching ways from start pointt to all other points on map.
         self.find_neighbours()
 
         frontier = collections.deque()
-        frontier.append(start)
+        frontier.append(self.start)
         self.came_from = {}
-        self.came_from[start] = None
+        self.came_from[self.start] = None
 
         while not len(frontier) == 0:
             current = frontier.popleft()
@@ -50,18 +57,18 @@ class Grid():
                     self.came_from[next] = current
         return self.came_from
 
-
-    def reconstruct_path(self, start, goal):
+    def reconstruct_path(self):
         # reconstracttion of the path from goal-pointt to the start-point
-        current = goal
+        current = self.goal
         self.path = []
-        while current != start:
+        while current != self.start:
             self.path.append(current)
             current = self.came_from[current]
-        self.path.append(start) # optional
-        self.path.reverse() # optional
+        self.path.append(self.start)
+        self.path.reverse()
         return self.path
 
+    
     def show_path_ongrid(self):
         for each in graph.path:
             x, y = each
@@ -80,15 +87,15 @@ class Grid():
 
 
 
-start = (0, 0)
-finish = (0, 4)
+
 
 graph = Grid()
 graph.create_wall(0, 3)
 graph.create_wall(1, 3)
 graph.create_wall(2, 3)
 graph.create_wall(3, 3)
+graph.set_start_goal(start=(0, 0), goal=(0, 4))
 
-graph.breadth_first_search(start)
-graph.reconstruct_path(start,finish)
+graph.breadth_first_search()
+graph.reconstruct_path()
 print(graph.show_path_ongrid())
--- temp.py
+++ temp.py
@@ -1,29 +1,6 @@
-grid = [[0 for _ in range(5)] for _ in range(5)]
-neighbours = {}
-height = 5-1
-width = 5-1
+x = ()
 
-walls = [(0, 3),(1, 3),(1, 2)]
+x = (5,4)
 
-def is_inside_field(vertex):
-    x, y = vertex
-    return 0 <= x <= width and 0 <= y <= height
-
-def is_passable(vertex):
-    return vertex not in walls 
-
-
-
-x=0
-for row in grid:
-    y=0
-    for elem in row:
-        if (x, y) not in walls:
-            neighbours[x,y] = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
-            neighbours[x,y] = list(filter(is_inside_field, neighbours[x,y]))
-            neighbours[x,y] = list(filter(is_passable, neighbours[x,y]))
-        y+=1
-    x+=1 
-
-
-print(neighbours)
+print(x)
+print(type(x))
